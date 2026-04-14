41da1eeb0ca9f92be6ca038f6cb9c082f90f5a92
Vizulation Games <iwannadothisnow12@gmail.com>
2026-04-13 20:56:42 -0400

    Update MenuManager.cs

--- MenuManager.cs
+++ MenuManager.cs
@@ -6,16 +6,16 @@ using UnityEngine.SceneManagement;
 public class MenuManager : MonoBehaviour
 {
     public List<Rooms> menuRooms;
-    public float flickerSpeed = 0.5f; // How fast lights change
+    public float flickerSpeed = 0.5f; 
     private float flickerTimer;
 
     void Start()
     {
-        // Find all rooms in the scene if not assigned
+       
         if (menuRooms.Count == 0)
             menuRooms = new List<Rooms>(Object.FindObjectsByType<Rooms>(FindObjectsSortMode.None));
 
-        // Ensure time is running for the animations/flickers
+     
         Time.timeScale = 1;
     }
 
@@ -33,19 +33,15 @@ public class MenuManager : MonoBehaviour
     {
         if (menuRooms.Count == 0) return;
 
-        // Pick a random room and toggle it visually
+   
         int randomIndex = Random.Range(0, menuRooms.Count);
 
-        // We use a custom method so we don't trigger the CharchterManager game logic
+       
         menuRooms[randomIndex].MenuToggle();
     }
 
     public void StartGame()
     {
-        // If your game is in a different scene:
-        // SceneManager.LoadScene("GameScene");
-
-        // If your game is in the same scene, just enable your CharchterManager 
-        // and disable the Menu UI/MenuManager.
+   
     }
 }
