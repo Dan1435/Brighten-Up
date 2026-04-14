5c5b732045fc3d027d24259f13a5b322d049cc75
Vizulation Games <iwannadothisnow12@gmail.com>
2026-04-13 20:57:48 -0400

    Update ScoreDisplay.cs

--- ScoreDisplay.cs
+++ ScoreDisplay.cs
@@ -10,17 +10,17 @@ public class ScoreDisplay : MonoBehaviour
         scoreText = GetComponent<TextMeshProUGUI>();
     }
 
-    // Combined into one single function
+
     public void UpdateScoreUI(int currentScore)
     {
         if (scoreText != null)
         {
             scoreText.text = currentScore.ToString();
 
-            // Visual feedback: turn yellow
+         
             scoreText.color = Color.yellow;
 
-            // Return to white after 0.1 seconds
+          
             Invoke("ResetColor", 0.1f);
         }
     }
