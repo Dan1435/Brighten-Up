af33587eba7a590386205442cfb9303529dec38a
Vizulation Games <iwannadothisnow12@gmail.com>
2026-04-13 20:56:05 -0400

    Update GameManager.cs

--- GameManager.cs
+++ GameManager.cs
@@ -2,23 +2,23 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    public GameObject menuCanvas; // Drag your Start Screen here
-    public CharchterManager charManager; // Drag the Manager with the timer here
+    public GameObject menuCanvas; 
+    public CharchterManager charManager; 
 
     void Start()
     {
-        // Stop time so nothing moves while at the menu
+   
         Time.timeScale = 0;
         menuCanvas.SetActive(true);
     }
 
     public void StartTheGame()
     {
-        // Hide the menu and unfreeze time
+    
         menuCanvas.SetActive(false);
         Time.timeScale = 1;
 
-        // Tell the other script to start the logic
+    
         charManager.isGameOver = false;
         charManager.NextRound();
     }
